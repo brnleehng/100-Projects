363c6f9294ccc923820c73434be70676ad27935a
slapvanilla <grasmick.lee@gmail.com>
2013-10-26 18:42:48 -0500

    Initial commit

---
+++ Classic Algorithms/CollatzConjecture.java
@@ -0,0 +1,37 @@
+import java.math.BigInteger;
+
+/**
+ * Finds the number of steps it takes for a number to go to 1 using the
+ * Collatz sequence algorithm.
+ */
+public class CollatzConjecture {
+
+    public static void main(String[] args) {
+
+        /* Verify arguments and initialize vars */
+        if (args.length == 0 || args.length > 1) usage();
+        BigInteger n = new BigInteger(args[0]);
+        int count  = 0;
+
+        /* Loop Collatz algorithm until n = 1 */
+        while (!n.equals(BigInteger.ONE)) {
+            if (n.mod(new BigInteger("2")) == BigInteger.ZERO) {
+                n = n.divide(new BigInteger("2"));
+            }
+            else {
+                n = n.multiply(new BigInteger("3"));
+                n = n.add(BigInteger.ONE);
+            }
+            count++;
+        }
+
+        /* Print the results */
+        System.out.printf("Steps required: %d", count);
+
+    }
+
+    private static void usage() {
+        System.out.print("usage: CollatzConjecture integer(>1)");
+        System.exit(0);
+    }
+}
